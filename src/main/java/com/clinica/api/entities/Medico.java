db2b987c0753075fc2d971ee7566bd9346b2d6c4
bielh <gabehpc@gmail.com>
2026-07-30 11:51:32 -0300

    Implementação da entidade Medico

--- src/main/java/com/clinica/api/entities/Medico.java
+++ src/main/java/com/clinica/api/entities/Medico.java
@@ -1,45 +1,46 @@
 package com.clinica.api.entities;
 
-import com.clinica.api.enums.Role;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "usuarios")
+@Table(name = "medicos")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-
-public class Usuario {
+public class Medico {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "O campo nome é obrigatório")
+    @NotBlank(message = "O nome do medico é obrigatório.")
     @Column(nullable = false)
     private String nome;
+
+    @NotBlank(message = "O crm do médico é obrigatório")
+    @Column(nullable = false ,unique = true)
+    private String crm;
 
     @NotBlank(message = "O campo E-mail é obrigatório")
     @Email(message = "O e-mail é invalido")
     @Column(nullable = false, unique = true)
     private String email;
 
-    @NotBlank(message = "O campo Senha é obrigatório")
-    @Column(nullable = false)
-    private String senha;
+    private String telefone;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private Role role;
+    @NotNull(message = "A especialidade é obrigatoria")
+    @ManyToOne
+    @JoinColumn(name = "especialidade_id", nullable = false)
+    private Especialidade especialidade;
 
     @Column(nullable = false)
     private Boolean ativo = true;
-
 }
