dccf2f9a16552b3bfc8866ac83643b108b2cf42e
bielh <gabehpc@gmail.com>
2026-08-10 12:09:18 -0300

    Implementação do JwtService

--- src/main/java/com/clinica/api/config/security/JwtService.java
+++ src/main/java/com/clinica/api/config/security/JwtService.java
@@ -1,4 +1,70 @@
 package com.clinica.api.config.security;
 
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
 public class JwtService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String gerarToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+                .claims(claims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 *60))
+                .signWith(getSecretKey())
+                .compact();
+    }
+
+    private SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String extrairUsername(String token) {
+        return extrairClaim(token, Claims::getSubject);
+    }
+
+    public boolean tokenValido(String token, UserDetails userDetails) {
+
+        String username = extrairUsername(token);
+
+        return username.equals(userDetails.getUsername())
+                && !tokenExpirado(token);
+    }
+
+    private boolean tokenExpirado(String token) {
+        return extrairClaim(token, Claims::getExpiration)
+                .before(new Date());
+    }
+
+    private <T> T extrairClaim(
+            String token,
+            Function<Claims, T> resolver) {
+
+        Claims claims = Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        return resolver.apply(claims);
+    }
+
+
 }
