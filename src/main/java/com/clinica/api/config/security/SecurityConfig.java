e5b00614f2696085005b330bea0b9453ff6a5a1e
bielh <gabehpc@gmail.com>
2026-08-11 13:02:12 -0300

    Correções, (Usuario Recepcionista ainda pode criar um novo usuario, isso será corrigido)

--- src/main/java/com/clinica/api/config/security/SecurityConfig.java
+++ src/main/java/com/clinica/api/config/security/SecurityConfig.java
@@ -32,7 +32,6 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/usuarios").permitAll()
                         .anyRequest().authenticated()
                 )
 
